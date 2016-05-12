f5eeb9382d35e31a1beaabe5b9dd83ec31b9d71a
Amo Wu <amo260@gmail.com>
2016-05-13 01:36:41 +0800

    temp: backup commit

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -1,13 +1,76 @@
+<style lang='scss' scope>
+  @import '~semantic-ui/dist/semantic.css';
+
+  #app {
+    height: 100%;
+    margin: 0;
+    background-color: #f3f4f5;
+  }
+  #profile {
+    margin-left: auto;
+  }
+</style>
+
 <script>
   import store from '../core/store'
 
   export default {
-    store
+    store,
+    ready () {
+      this.$http({
+        url: 'https://raw.githubusercontent.com/amowu/resume/gh-pages/resume.json',
+        method: 'GET'
+      }).then(response => {
+        console.log(response)
+      }, response => {
+        console.log(response)
+      })
+    }
   }
 </script>
 
-<template>
-  <div>
-    <router-view></router-view>
-  </div>
+<template lang='jade'>
+  #app.ui.two.column.stackable.grid
+    .four.wide.column
+      //- TODO: ProfileCard component
+      #profile.ui.card
+        //- profile avatar
+        .image
+          img(src='http://www.gravatar.com/avatar/778858b33950746e7049bfc33e94ac50?s=350')
+        //- profile info
+        .content
+          .header Amo Wu
+          .meta Front-End Engineer
+          .description F2E formosanus.
+          .ui.list
+            .item
+              i.marker.icon
+              .content New Taipei City, Taiwan
+            .item
+              i.mail.icon
+              .content
+                a(href='mailto:amo260@gmail.com') amo260@gmail.com
+            .item
+              i.linkify.icon
+              .content
+                a(href='http://amowu.com') amowu.com
+            .item
+              i.write.icon
+              .content
+                a(href='http://blog.amowu.com') blog.amowu.com
+            .item
+              i.world.icon
+              .content Chinese, English, Japanese
+        //- social media
+        .content
+          a.ui.circular.basic.icon.button(href='https://www.linkedin.com/in/amowu', target='_black')
+            i.linkedin.icon
+          a.ui.circular.basic.icon.button(href='https://github.com/amowu', target='_black')
+            i.icon.github
+          a.ui.circular.basic.icon.button(href='https://twitter.com/amo_wu', target='_black')
+            i.twitter.icon
+          a.ui.circular.basic.icon.button(href='https://www.behance.net/amo_wu', target='_black')
+            i.icon.behance
+    .twelve.wide.column
+      router-view
 </template>
--- src/pages/Resume/index.vue
+++ src/pages/Resume/index.vue
@@ -1,20 +1,39 @@
-<script>
-  export default {
-    ready () {
-      this.$http({
-        url: 'https://raw.githubusercontent.com/amowu/resume/gh-pages/resume.json',
-        method: 'GET'
-      }).then(response => {
-        console.log(response)
-      }, response => {
-        console.log(response)
-      })
-    }
-  }
-</script>
-
-<template>
-  <div>
-    Resume
-  </div>
+<template lang='jade'>
+  //- TODO: Resume component
+  .ui.segment
+    h4.ui.red.ribbon.label
+      i.user.icon
+      | ABOUT
+    ul.ui.list
+      li 三年以上 Web & Game 相關工作經驗
+      li 熟悉 Flash、HTML5 等 RIA 相關技術
+      li 專注於 Front-end Web Development、Serverless Cloud Services、DevOps
+    h4.ui.blue.ribbon.label
+      i.suitcase.icon
+      | WORK EXPERIENCE
+    h4.ui.header
+      .content
+        strong Senior Programmer
+        | ,&nbsp;
+        a(href='http://corp.gamania.com/') Gamania Digital Entertainment
+        .sub.header
+          span Apr, 2011 - Aug, 2014 <i class='clock icon'></i> 3 years 4 months
+    ul.ui.list
+      li 利用新人試用期三個月的時間，負責開發首次嘗試的跨平台 HTML5 遊戲《Gu Morning》， 之後成為部門第一款擁有收入來源的上市遊戲。
+      li 完整參與長達一年的大型 Web MMO RPG《末日少女》專案開發，上市成績最高同時在線人數 8000 人。
+      li 主動協助團隊建構 DevOps 自動化開發環境，獲得團隊與主管讚賞。
+    h4.ui.orange.ribbon.label
+      i.keyboard.icon
+      | SKILLS
+    p
+    h4.ui.green.ribbon.label
+      i.student.icon
+      | EDUCATION
+    h4.ui.header
+      .content
+        strong Computer Science & Information Engineering, Bachelor
+        | ,&nbsp;
+        a(href='http://www.ntut.edu.tw/') National Taipei University of Technology
+        .sub.header
+          span Aug, 2005 - Jun, 2009
 </template>
